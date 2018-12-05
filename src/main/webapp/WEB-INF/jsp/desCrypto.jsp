b9e7bfb1930b5aa6859eceb7f8e1467cbc9bd330
mxp <1395124893@qq.com>
2018-12-05 21:57:35 +0800

    json 格式话

--- src/main/webapp/WEB-INF/jsp/desCrypto.jsp
+++ src/main/webapp/WEB-INF/jsp/desCrypto.jsp
@@ -14,11 +14,22 @@
     <script type="text/javascript" src="${pageContext.request.contextPath}/static/js/desCrypto.js"></script>
     <link rel="stylesheet" href="${pageContext.request.contextPath}/static/bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="${pageContext.request.contextPath}/static/css/style.css"/>
+    <style>
+
+        #exampleTextarea {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
+    .string { color: green; }
+    .number { color: darkorange; }
+    .boolean { color: blue; }
+    .null { color: magenta; }
+    .key { color: red; }
+    </style>
 </head>
 <body>
 <div class="container" style="overflow-x:visible;overflow-y:visible;margin-top: 10px">
-
     <div class="starter-template">
+        <pre id="result">
+
+        </pre>
         <div class="row">
             <div class="col-md-12">
                 <div class="form-group" >
@@ -29,7 +40,7 @@
             <div class="col-md-6">
                 <div class="form-group" class="col-md-6">
                     <label for="exampleTextarea">输入域</label>
-                    <textarea id="exampleTextarea" style="resize:none;height: 500px;overflow-y:auto" class="form-control" rows="3"></textarea>
+                    <textarea id="exampleTextarea" onblur="formatJson()"  style="resize:none;height: 500px;overflow-y:auto" class="form-control" rows="3"></textarea>
                 </div>
             </div>
             <div class="col-md-6">
--- src/main/webapp/static/js/desCrypto.js
+++ src/main/webapp/static/js/desCrypto.js
@@ -21,80 +21,30 @@ function enDes() {
         }
     })
 }
-function formatJson(json, options) {
-    var reg = null,
-        formatted = '',
-        pad = 0,
-        PADDING = '    '; // one can also use '\t' or a different number of spaces
-    // optional settings
-    options = options || {};
-    // remove newline where '{' or '[' follows ':'
-    options.newlineAfterColonIfBeforeBraceOrBracket = (options.newlineAfterColonIfBeforeBraceOrBracket === true) ? true : false;
-    // use a space after a colon
-    options.spaceAfterColon = (options.spaceAfterColon === false) ? false : true;
-
-    // begin formatting...
-
-    // make sure we start with the JSON as a string
-    if (typeof json !== 'string') {
-        json = JSON.stringify(json);
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
     }
-    // parse and stringify in order to remove extra whitespace
-    json = JSON.parse(json);
-    json = JSON.stringify(json);
-
-    // add newline before and after curly braces
-    reg = /([\{\}])/g;
-    json = json.replace(reg, '\r\n$1\r\n');
-
-    // add newline before and after square brackets
-    reg = /([\[\]])/g;
-    json = json.replace(reg, '\r\n$1\r\n');
-
-    // add newline after comma
-    reg = /(\,)/g;
-    json = json.replace(reg, '$1\r\n');
-
-    // remove multiple newlines
-    reg = /(\r\n\r\n)/g;
-    json = json.replace(reg, '\r\n');
-
-    // remove newlines before commas
-    reg = /\r\n\,/g;
-    json = json.replace(reg, ',');
-
-    // optional formatting...
-    if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
-        reg = /\:\r\n\{/g;
-        json = json.replace(reg, ':{');
-        reg = /\:\r\n\[/g;
-        json = json.replace(reg, ':[');
-    }
-    if (options.spaceAfterColon) {
-        reg = /\:/g;
-        json = json.replace(reg, ': ');
-    }
-
-    $.each(json.split('\r\n'), function(index, node) {
-        var i = 0,
-            indent = 0,
-            padding = '';
-
-        if (node.match(/\{$/) || node.match(/\[$/)) {
-            indent = 1;
-        } else if (node.match(/\}/) || node.match(/\]/)) {
-            if (pad !== 0) {
-                pad -= 1;
+    json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
             }
-        } else {
-            indent = 0;
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
         }
-
-        for (i = 0; i < pad; i++) {
-            padding += PADDING;
-        }
-        formatted += padding + node + '\r\n';
-        pad += indent;
+        return '<span class="' + cls + '">' + match + '</span>';
     });
-    return formatted;
-};
+
+}
+function formatJson() {
+    var json = $("#exampleTextarea").val();
+    console.log(syntaxHighlight(json));
+    $('#result').html(syntaxHighlight(json));
+}
